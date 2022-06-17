a67e31e90e5ea4710aadc3e764192c1075f2ab55
Emmanuel Ahuron <65813159+Ahuron29@users.noreply.github.com>
2022-06-17 23:32:46 +0000

    Executes

--- execute.c
+++ execute.c
@@ -8,17 +8,17 @@
  */
 int is_path(char *command)
 {
-  int i;
+	int i;
 
-  i = 0;
-  while (command[i] != '\0')
-    {
-      if (command[i] == '/')
-	return (1);
-      i++;
-    }
+	i = 0;
+	while (command[i] != '\0')
+	{
+		if (command[i] == '/')
+			return (1);
+		i++;
+	}
 
-  return (0);
+		return (0);
 }
 
 /**
@@ -29,34 +29,34 @@ int is_path(char *command)
  */
 int exec_builtins(arg_inventory_t *arginv)
 {
-  int i, retval;
-  /* old_stdout */
-  char *str, **commands;
-  builtins_t builtins_list[] = {
+	int i, retval;
+	/* old_stdout */
+	char *str, **commands;
+	builtins_t builtins_list[] = {
 
-				{"env", _env}, {"setenv", _setenv},
-				{"unsetenv", _unsetenv}, {"history", _history}, {"cd", _cd},
-				{"alias", _alias}, {"unalias", _unalias}, {"help", shell_help},
-				{"exit", shell_exit},
-				{NULL, NULL}
-  };
+		{"env", _env}, {"setenv", _setenv},
+		{"unsetenv", _unsetenv}, {"history", _history}, {"cd", _cd},
+		{"alias", _alias}, {"unalias", _unalias}, {"help", shell_help},
+		{"exit", shell_exit},
+		{NULL, NULL}
+	};
 
-  retval = EXT_FAILURE;
-  commands = (char **)arginv->commands;
+	retval = EXT_FAILURE;
+	commands = (char **)arginv->commands;
 
-  /* old_stdout = redirect_output(arginv, 0); */
+	/* old_stdout = redirect_output(arginv, 0); */
 
-  for (i = 0; ((str = builtins_list[i].command) != NULL); i++)
-    {
-      if (_strcmp(str, commands[0]) == 0)
+	for (i = 0; ((str = builtins_list[i].command) != NULL); i++)
 	{
-	  retval = builtins_list[i].builtin_func(arginv);
-	  break;
+		if (_strcmp(str, commands[0]) == 0)
+		{
+			retval = builtins_list[i].builtin_func(arginv);
+			break;
+		}
 	}
-    }
 
-  arginv->exit_status = retval;
-  return (retval);
+	arginv->exit_status = retval;
+	return (retval);
 }
 
 /**
@@ -67,16 +67,16 @@ int exec_builtins(arg_inventory_t *arginv)
  * @arginv: arg inventory to free
  */
 void exec_error_exit(char *msg, char *command, char **_environ,
-		     arg_inventory_t *arginv)
+		arg_inventory_t *arginv)
 {
-  delete_pipeline(&arginv->pipeline);
-  delete_parser(&arginv->parser);
-  delete_tokens(&arginv->tokens);
-  free(command);
-  free_paths(_environ);
-  freeall(arginv);
-  _perror(msg);
-  exit(1);
+	delete_pipeline(&arginv->pipeline);
+	delete_parser(&arginv->parser);
+	delete_tokens(&arginv->tokens);
+	free(command);
+	free_paths(_environ);
+	freeall(arginv);
+	_perror(msg);
+	exit(1);
 }
 
 /**
@@ -88,25 +88,25 @@ void exec_error_exit(char *msg, char *command, char **_environ,
  */
 pid_t exec_path(char *command, arg_inventory_t *arginv)
 {
-  pid_t pid;
-  char **_environ;
+	pid_t pid;
+	char **_environ;
 
-  pid = fork();
-  if (pid < 0)
-    {
-      _perror("Critical error: unable to fork()!\n");
-      exit(1);
-    }
+	pid = fork();
+	if (pid < 0)
+	{
+		_perror("Critical error: unable to fork()!\n");
+		exit(1);
+	}
 
-  if (pid == 0)
-    {
-      _environ = link_to_dpointer(arginv->envlist);
+	if (pid == 0)
+	{
+		_environ = link_to_dpointer(arginv->envlist);
 
-      if (execve(command, (char **)arginv->commands, _environ) < 0)
-	exec_error_exit("No Command\n", command, _environ, arginv);
-    }
-  free(command);
-  return (pid);
+		if (execve(command, (char **)arginv->commands, _environ) < 0)
+			exec_error_exit("No Command\n", command, _environ, arginv);
+	}
+	free(command);
+	return (pid);
 }
 
 /**
@@ -117,35 +117,34 @@ pid_t exec_path(char *command, arg_inventory_t *arginv)
  */
 pid_t execute(arg_inventory_t *arginv)
 {
-  env_t *envlist;
-  char **commands;
-  char *path, *command;
-  char **paths;
+	env_t *envlist;
+	char **commands;
+	char *path, *command;
+	char **paths;
 
-  envlist = arginv->envlist;
+	envlist = arginv->envlist;
 
-  commands = (char **)arginv->commands;
+	commands = (char **)arginv->commands;
 
-  command = safe_malloc(sizeof(char) * BUFSIZE);
-  command = _strcpy(command, *commands);
+	command = safe_malloc(sizeof(char) * BUFSIZE);
+	command = _strcpy(command, *commands);
 
-  if (exec_builtins(arginv) == EXT_FAILURE)
-    {
-      if (is_path(command))
+	if (exec_builtins(arginv) == EXT_FAILURE)
 	{
-	  return (exec_path(command, arginv));
+		if (is_path(command))
+		{
+			return (exec_path(command, arginv));
+		}
+		else
+		{
+			path = safe_malloc(sizeof(char) * BUFSIZE);
+			locate_path(path, envlist);
+			paths = tokenize_path(path);
+			cat_path(paths, command);
+			free_paths(paths);
+			free(path);
+			return (exec_path(command, arginv));
+		}
 	}
-      else
-	{
-	  path = safe_malloc(sizeof(char) * BUFSIZE);
-	  locate_path(path, envlist);
-	  paths = tokenize_path(path);
-	  cat_path(paths, command);
-	  free_paths(paths);
-	  free(path);
-	  return (exec_path(command, arginv));
-	}
-    }
-  free(command);
-  return (-1);
-}
+	free(command);
+	return (-1);
