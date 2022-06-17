3ff4c89bb879e396e6ab51d8d9f8ff88313abc81
Emmanuel Ahuron <65813159+Ahuron29@users.noreply.github.com>
2022-06-17 23:36:24 +0000

    Env 1

--- env1.c
+++ env1.c
@@ -8,19 +8,19 @@
  */
 unsigned int link_count(env_t *head)
 {
-  env_t *tmp;
-  unsigned int count;
+	env_t *tmp;
+	unsigned int count;
 
-  tmp = head;
-  count = 0;
+	tmp = head;
+	count = 0;
 
-  while (tmp != NULL)
-    {
-      tmp = tmp->next;
-      count++;
-    }
+	while (tmp != NULL)
+	{
+		tmp = tmp->next;
+		count++;
+	}
 
-  return (count);
+	return (count);
 }
 
 /**
@@ -30,22 +30,22 @@ unsigned int link_count(env_t *head)
  */
 env_t *env_list(void)
 {
-  int i;
-  env_t *head;
-  char **variable;
+	int i;
+	env_t *head;
+	char **variable;
 
-  head = NULL;
-  for (i = 0; environ[i] != NULL; i++)
-    {
-      variable = separate_string(environ[i]);
-      if (add_node_env(&head, variable[0], variable[1]) == NULL)
-	return (NULL);
-      free(variable[0]);
-      free(variable[1]);
-      free(variable);
-    }
+	head = NULL;
+	for (i = 0; environ[i] != NULL; i++)
+	{
+		variable = separate_string(environ[i]);
+		if (add_node_env(&head, variable[0], variable[1]) == NULL)
+			return (NULL);
+		free(variable[0]);
+		free(variable[1]);
+		free(variable);
+	}
 
-  return (head);
+	return (head);
 }
 
 /**
@@ -56,34 +56,34 @@ env_t *env_list(void)
  */
 char **link_to_dpointer(env_t *head)
 {
-  int i;
-  unsigned int count, len1, len2, lennew;
-  char **dpointer, *var, *val, *new_val;
-  env_t *tmp;
+	int i;
+	unsigned int count, len1, len2, lennew;
+	char **dpointer, *var, *val, *new_val;
+	env_t *tmp;
 
-  count = link_count(head);
-  dpointer = malloc(sizeof(char *) * (count + 1));
+	count = link_count(head);
+	dpointer = malloc(sizeof(char *) * (count + 1));
 
-  tmp = head;
-  i = 0;
-  while (tmp != NULL)
-    {
-      var = tmp->var;
-      val = tmp->val;
-      len1 = _strlen(var);
-      len2 = _strlen(val);
+	tmp = head;
+	i = 0;
+	while (tmp != NULL)
+	{
+		var = tmp->var;
+		val = tmp->val;
+		len1 = _strlen(var);
+		len2 = _strlen(val);
 
-      lennew = len1 + len2 + 2;
-      new_val = safe_malloc(lennew * sizeof(char));
+		lennew = len1 + len2 + 2;
+		new_val = safe_malloc(lennew * sizeof(char));
 
-      _strncat(new_val, var, len1);
-      _strncat(new_val, "=", 1);
-      _strncat(new_val, val, len2);
-      dpointer[i] = new_val;
-      tmp = tmp->next;
-      i++;
-    }
-  dpointer[i] = NULL;
+		_strncat(new_val, var, len1);
+		_strncat(new_val, "=", 1);
+		_strncat(new_val, val, len2);
+		dpointer[i] = new_val;
+		tmp = tmp->next;
+		i++;
+	}
+	dpointer[i] = NULL;
 
-  return (dpointer);
+	return (dpointer);
 }
