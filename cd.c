c79357e6fcd01612f657773dbfd817728671d937
Emmanuel Ahuron <65813159+Ahuron29@users.noreply.github.com>
2022-06-17 23:40:24 +0000

    CDC

--- cd.c
+++ cd.c
@@ -9,33 +9,34 @@
  */
 char *file_path(char **commands, env_t *envlist)
 {
-  env_t *fetched_home, *fetched_old;
-  int hyphen;
-  char *path;
+	env_t *fetched_home, *fetched_old;
+	int hyphen;
+	char *path;
 
-  path = safe_malloc(1024);
+	path = safe_malloc(1024);
 
-  fetched_home = fetch_node(envlist, "HOME");
-  fetched_old = fetch_node(envlist, "OLDPWD");
+	fetched_home = fetch_node(envlist, "HOME");
+	fetched_old = fetch_node(envlist, "OLDPWD");
 
-  if (commands[1] != NULL)
-    hyphen = _strncmp(commands[1], "-", 1);
+	if (commands[1] != NULL)
+		hyphen = _strncmp(commands[1], "-", 1);
 
-  if (commands[1] == NULL)
-    path = _strcpy(path, fetched_home->val);
-  else if (hyphen == 0)
-    path = _strcpy(path, fetched_old->val);
-  else if (commands[1][0] == '/')
-    path = _strcpy(path, commands[1]);
-  else
-    {
-      getcwd(path, 1024);
-      _strncat(path, "/", 1);
-      _strncat(path, commands[1], _strlen(commands[1]));
-    }
+	if (commands[1] == NULL)
+		path = _strcpy(path, fetched_home->val);
+	else if (hyphen == 0)
+		path = _strcpy(path, fetched_old->val);
+	else if (commands[1][0] == '/')
+		path = _strcpy(path, commands[1]);
+	else
+	{
+		getcwd(path, 1024);
+		_strncat(path, "/", 1);
+		_strncat(path, commands[1], _strlen(commands[1]));
+	}
 
-  return (path);
+	return (path);
 }
+
 
 /**
  * _cd - builtin cd function
@@ -45,36 +46,36 @@ char *file_path(char **commands, env_t *envlist)
  */
 int _cd(arg_inventory_t *arginv)
 {
-  char *path, *oldpwd, *pwd, **commands;
-  int check;
+	char *path, *oldpwd, *pwd, **commands;
+	int check;
 
-  oldpwd = safe_malloc(1024);
-  pwd = safe_malloc(1024);
+	oldpwd = safe_malloc(1024);
+	pwd = safe_malloc(1024);
 
-  getcwd(oldpwd, 1024);
+	getcwd(oldpwd, 1024);
 
-  commands = (char **)arginv->commands;
+	commands = (char **)arginv->commands;
 
-  path = file_path(commands, arginv->envlist);
+	path = file_path(commands, arginv->envlist);
 
-  check = chdir(path);
+	check = chdir(path);
 
-  if (check == -1)
-    {
-      free(oldpwd);
-      free(pwd);
-      free(path);
-      return (2);
-    }
-  else
-    {
-      getcwd(pwd, 1024);
-      modify_node_env(&arginv->envlist, "PWD", pwd);
-      modify_node_env(&arginv->envlist, "OLDPWD", oldpwd);
-    }
-  free(oldpwd);
-  free(pwd);
-  free(path);
+	if (check == -1)
+	{
+		free(oldpwd);
+		free(pwd);
+		free(path);
+		return (2);
+	}
+	else
+	{
+		getcwd(pwd, 1024);
+		modify_node_env(&arginv->envlist, "PWD", pwd);
+		modify_node_env(&arginv->envlist, "OLDPWD", oldpwd);
+	}
+	free(oldpwd);
+	free(pwd);
+	free(path);
 
-  return (0);
+	return (0);
 }
