a9938a21efe26d045b83250e6c58985daa64e512
Emmanuel Ahuron <65813159+Ahuron29@users.noreply.github.com>
2022-06-17 23:17:30 +0000

    History

--- history.c
+++ history.c
@@ -8,43 +8,44 @@
  */
 history_t *history_list(arg_inventory_t *arginv)
 {
-  history_t *head;
-  char *file, *buffer;
-  int fd;
-  size_t iterations = 1, charcount = 0, i = 0;
+	history_t *head;
+	char *file, *buffer;
+	int fd;
+	size_t iterations = 1, charcount = 0, i = 0;
 
-  head = NULL;
-  file = arginv->history_file;
-  fd = open(file, O_RDONLY);
-  buffer = safe_malloc(sizeof(char) * BUFSIZE);
-  if (fd == -1 || buffer == NULL)
-    {
-      free(buffer);
-      return (head);
-    }
-  while (TRUE)
-    {
-      i = read(fd, (buffer + charcount), BUFSIZE);
-      if (i <= 0)
-	break;
-      charcount += i;
-      if (i % BUFSIZE == 0)
+	head = NULL;
+	file = arginv->history_file;
+	fd = open(file, O_RDONLY);
+	buffer = safe_malloc(sizeof(char) * BUFSIZE);
+	if (fd == -1 || buffer == NULL)
 	{
-	  iterations++;
-	  buffer = _realloc(buffer, charcount, (BUFSIZE * iterations));
+		free(buffer);
+		return (head);
 	}
-      else
-	break;
-    }
-  close(fd);
-  if (charcount == 0)
-    {
-      free(buffer);
-      return (head);
-    }
-  head = init_history(head, buffer);
-  return (head);
+	while (TRUE)
+	{
+		i = read(fd, (buffer + charcount), BUFSIZE);
+		if (i <= 0)
+			break;
+		charcount += i;
+		if (i % BUFSIZE == 0)
+		{
+			iterations++;
+			buffer = _realloc(buffer, charcount, (BUFSIZE * iterations));
+		}
+		else
+			break;
+	}
+	close(fd);
+	if (charcount == 0)
+	{
+		free(buffer);
+		return (head);
+	}
+	head = init_history(head, buffer);
+	return (head);
 }
+
 /**
  * init_history - initializes history using history file converted to buffer
  * @head: head of linked list of history commands
@@ -54,28 +55,28 @@ history_t *history_list(arg_inventory_t *arginv)
  */
 history_t *init_history(history_t *head, char *buffer)
 {
-  int marker, sublen, j, i = 0;
-  char *temp;
+	int marker, sublen, j, i = 0;
+	char *temp;
 
-  while (buffer[i] != '\0')
-    {
-      marker = i;
-      sublen = 0;
-      while (buffer[i] != '\n')
-	sublen++, i++;
-      temp = safe_malloc(sizeof(char) * (sublen + 2));
-      j = 0;
-      while (j <= sublen)
+	while (buffer[i] != '\0')
 	{
-	  temp[j] = buffer[marker];
-	  j++, marker++;
+		marker = i;
+		sublen = 0;
+		while (buffer[i] != '\n')
+			sublen++, i++;
+		temp = safe_malloc(sizeof(char) * (sublen + 2));
+		j = 0;
+		while (j <= sublen)
+		{
+			temp[j] = buffer[marker];
+			j++, marker++;
+		}
+		add_node_history(&head, temp);
+		i++;
+		free(temp);
 	}
-      add_node_history(&head, temp);
-      i++;
-      free(temp);
-    }
-  free(buffer);
-  return (head);
+	free(buffer);
+	return (head);
 }
 
 /**
@@ -87,37 +88,37 @@ history_t *init_history(history_t *head, char *buffer)
  */
 history_t *add_node_history(history_t **head, char *command)
 {
-  history_t *new_node, *temp;
-  unsigned int i = 1;
+	history_t *new_node, *temp;
+	unsigned int i = 1;
 
-  new_node = malloc(sizeof(history_t));
-  if (new_node == NULL)
-    return (NULL);
-  new_node->command = _strdup(command);
-  new_node->next = NULL;
-  new_node->number = 0;
-  if (!*head)
-    *head = new_node;
-  else
-    {
-      temp = *head;
-
-      while (temp->next)
+	new_node = malloc(sizeof(history_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->command = _strdup(command);
+	new_node->next = NULL;
+	new_node->number = 0;
+	if (!*head)
+		*head = new_node;
+	else
 	{
-	  temp = temp->next;
-	  i++;
-	}
-      if (i > 4095)
-	{
-	  free_history(*head);
-	  *head = new_node;
-	  return (new_node);
-	}
-      new_node->number = i;
-      temp->next = new_node;
-    }
+		temp = *head;
 
-  return (new_node);
+		while (temp->next)
+		{
+			temp = temp->next;
+			i++;
+		}
+		if (i > 4095)
+		{
+			free_history(*head);
+			*head = new_node;
+			return (new_node);
+		}
+		new_node->number = i;
+		temp->next = new_node;
+	}
+
+	return (new_node);
 }
 
 /**
@@ -128,15 +129,16 @@ history_t *add_node_history(history_t **head, char *command)
  */
 int file_history(arg_inventory_t *arginv)
 {
-  char *history_text;
+	char *history_text;
 
-  history_text = history_to_string(arginv->history);
-  trunc_text_to_file(arginv->history_file, history_text);
+	history_text = history_to_string(arginv->history);
+	trunc_text_to_file(arginv->history_file, history_text);
 
-  free(history_text);
+	free(history_text);
 
-  return (0);
+	return (0);
 }
+
 /**
  * history_to_string - converts history linked list to string
  * @head: head of linked list of history
@@ -145,24 +147,24 @@ int file_history(arg_inventory_t *arginv)
  */
 char *history_to_string(history_t *head)
 {
-  history_t *temp = head;
-  char *string;
-  unsigned int size = 0;
+	history_t *temp = head;
+	char *string;
+	unsigned int size = 0;
 
-  while (temp)
-    {
-      size += _strlen(temp->command);
-      temp = temp->next;
-    }
+	while (temp)
+	{
+		size += _strlen(temp->command);
+		temp = temp->next;
+	}
 
-  string = safe_malloc(sizeof(char) * (size + 1));
+	string = safe_malloc(sizeof(char) * (size + 1));
 
-  temp = head;
-  while (temp)
-    {
-      _strncat(string, temp->command, _strlen(temp->command));
-      temp = temp->next;
-    }
+	temp = head;
+	while (temp)
+	{
+		_strncat(string, temp->command, _strlen(temp->command));
+		temp = temp->next;
+	}
 
-  return (string);
+	return (string);
 }
